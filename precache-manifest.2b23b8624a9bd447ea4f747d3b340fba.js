21c7c9cb8adf61e4d1e37b6897bd1e1c68ce4a76
ghonau <gholamreza.fathpour@gmail.com>
2019-12-29 19:19:09 +1030

    Updates

---
+++ precache-manifest.2b23b8624a9bd447ea4f747d3b340fba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d239cef2f172fb115fb360108538676b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "222b8daaf90c4d56782c",
+    "url": "/monsters-rolodex/static/css/main.2c34ef57.chunk.css"
+  },
+  {
+    "revision": "540273238f239017da25",
+    "url": "/monsters-rolodex/static/js/2.d402a341.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d402a341.chunk.js.LICENSE"
+  },
+  {
+    "revision": "222b8daaf90c4d56782c",
+    "url": "/monsters-rolodex/static/js/main.5616fcc7.chunk.js"
+  },
+  {
+    "revision": "b6333cf1d7701baa75ef",
+    "url": "/monsters-rolodex/static/js/runtime-main.2735d42a.js"
+  }
+]);
